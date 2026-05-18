37583ddf1e9100b0c7c2bf6c686bf76438de3d51
HieuLV03 <levanhieu14102k3@gmail.com>
2026-05-18 21:17:55 +0700

    add google verify head

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="iMhkqfnYHYPZ2e7ZhvNa8URs0nteVRjtS03F9CKa0sU"
+      />
+    </>
+  );
+}
