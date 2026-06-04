2d5ba502e0989b1f9b163df70f76f28d6710408a
HieuLV03 <levanhieu14102k3@gmail.com>
2026-06-04 09:55:19 +0700

    jj

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -41,59 +41,59 @@ export default function RootLayout({ children }) {
       lang="vi"
       className={`${inter.variable} ${playfair.variable} ${poppins.variable}`}
     >
-      <head>
-        {/* TikTok Pixel */}
-        <Script id="tiktok-pixel" strategy="afterInteractive">
-          {`
-          !function (w, d, t) {
-            w.TiktokAnalyticsObject=t;
-            var ttq=w[t]=w[t]||[];
+   <head>
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+      !function (w, d, t) {
+        w.TiktokAnalyticsObject=t;
+        var ttq=w[t]=w[t]||[];
 
-            ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+        ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
 
-            ttq.setAndDefer=function(t,e){
-              t[e]=function(){
-                t.push([e].concat(Array.prototype.slice.call(arguments,0)))
-              }
-            };
+        ttq.setAndDefer=function(t,e){
+          t[e]=function(){
+            t.push([e].concat(Array.prototype.slice.call(arguments,0)))
+          }
+        };
 
-            for(var i=0;i<ttq.methods.length;i++){
-              ttq.setAndDefer(ttq,ttq.methods[i]);
-            }
+        for(var i=0;i<ttq.methods.length;i++){
+          ttq.setAndDefer(ttq,ttq.methods[i]);
+        }
 
-            ttq.instance=function(t){
-              for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++){
-                ttq.setAndDefer(e,ttq.methods[n]);
-              }
-              return e;
-            };
+        ttq.instance=function(t){
+          for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++){
+            ttq.setAndDefer(e,ttq.methods[n]);
+          }
+          return e;
+        };
 
-            ttq.load=function(e,n){
-              var r="https://analytics.tiktok.com/i18n/pixel/events.js";
-              ttq._i=ttq._i||{};
-              ttq._i[e]=[];
-              ttq._i[e]._u=r;
-              ttq._t=ttq._t||{};
-              ttq._t[e]=+new Date;
-              ttq._o=ttq._o||{};
-              ttq._o[e]=n||{};
+        ttq.load=function(e,n){
+          var r="https://analytics.tiktok.com/i18n/pixel/events.js";
+          ttq._i=ttq._i||{};
+          ttq._i[e]=[];
+          ttq._i[e]._u=r;
+          ttq._t=ttq._t||{};
+          ttq._t[e]=+new Date;
+          ttq._o=ttq._o||{};
+          ttq._o[e]=n||{};
 
-              var s=document.createElement("script");
-              s.type="text/javascript";
-              s.async=true;
-              s.src=r+"?sdkid="+e+"&lib="+t;
+          var s=document.createElement("script");
+          s.type="text/javascript";
+          s.async=true;
+          s.src=r+"?sdkid="+e+"&lib="+t;
 
-              var x=document.getElementsByTagName("script")[0];
-              x.parentNode.insertBefore(s,x);
-            };
+          var x=document.getElementsByTagName("script")[0];
+          x.parentNode.insertBefore(s,x);
+        };
 
-            ttq.load('D8GE2OJC77U3N4JA967G');
-            ttq.page();
-          }(window, document, 'ttq');
-          `}
-        </Script>
-      </head>
-
+        ttq.load('D8GE2OJC77U3N4JA967G');
+        ttq.page();
+      }(window, document, 'ttq');
+      `,
+    }}
+  />
+</head>
       <body className="appBody">
         <ClientLayout>{children}</ClientLayout>
       </body>
