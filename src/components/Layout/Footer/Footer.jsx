3052a7230be8616ac7a4394d4bda49d0208b9438
HieuLV03 <levanhieu14102k3@gmail.com>
2026-06-29 15:53:33 +0700

    j

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -16,6 +16,33 @@ export default function Footer() {
           </p>
         </div>
 
+        {/* COMPANY INFO */}
+        <div className="footerCompany">
+          <h3>CÔNG TY TNHH HISU BEAUTY</h3>
+
+          <p>
+            <strong>🏢 Trụ sở chính:</strong><br />
+            354/47 Quốc lộ 1, P. Bình Tân, TP.HCM
+          </p>
+
+          <p>
+            <strong>🏥 Cơ sở 2:</strong><br />
+            15A Sông Đà, P. Tân Sơn Hòa, TP.HCM
+          </p>
+
+          <p>
+            <strong>📞 Điện thoại:</strong> 0372 089 821
+          </p>
+
+          <p>
+            <strong>📧 Email:</strong> thammyvienhisu@gmail.com
+          </p>
+
+          <p>
+            <strong>🌐 Website:</strong> www.hisu.com
+          </p>
+        </div>
+
         {/* LINKS */}
         <div className="footerLinks">
           <h3>Liên kết</h3>
@@ -26,35 +53,33 @@ export default function Footer() {
           <Link href="/booking">Đặt lịch</Link>
         </div>
 
-        {/* CONTACT */}
+        {/* SOCIAL / HOTLINE */}
         <div className="footerContact">
-          <h3>Liên hệ</h3>
+          <h3>Hotline hỗ trợ</h3>
 
           <p>📞 0372 089 821</p>
-          <p>📧 thammyvienhisu@gmail.com</p>
 
-          <div className="footerAddress">
-            <p>
-              <strong>🏢 Trụ sở:</strong> 354/47 Quốc lộ 1, P. Bình Tân, TP.HCM
-            </p>
-            <p>
-              <strong>🏥 Cơ sở:</strong> 15A Sông Đà, P. Tân Sơn Hòa, TP.HCM
-            </p>
-          </div>
-          <h3>Mạng xã hội FACEBOOK</h3>
+          <p>💬 Tư vấn 24/7</p>
+
+          <h3>Mạng xã hội</h3>
 
           <div className="social">
-            <a href="https://www.facebook.com/profile.php?id=61564510092133">Thẩm mỹ viện HISU</a>
+            <a href="https://www.facebook.com/profile.php?id=61564510092133">
+              Facebook HISU Beauty
+            </a>
           </div>
-             <div className="social">
-            <a href="https://www.facebook.com/profile.php?id=61588684948414">Dịch vụ chuyên làm hồng vùng kín</a>
+
+          <div className="social">
+            <a href="https://www.facebook.com/profile.php?id=61588684948414">
+              Fanpage dịch vụ
+            </a>
           </div>
         </div>
 
       </div>
 
       <div className="footerBottom">
-        © {new Date().getFullYear()} HISU. All rights reserved.
+        © {new Date().getFullYear()} HISU Beauty. All rights reserved.
       </div>
     </footer>
   );
